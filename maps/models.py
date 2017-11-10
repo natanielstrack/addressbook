5260c4ae51a22055eb7890da39bc103659524a10
Nataniel Strack <nataniel.strack@gmail.com>
2017-11-10 19:57:16 -0200

    Removing fusiontables insert. Adding address field. Adding googlemaps package.

--- maps/models.py
+++ maps/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Address(models.Model):
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    address = models.CharField(max_length=250)
--- maps/views.py
+++ maps/views.py
@@ -1,9 +1,5 @@
-import http.client
-from httplib2 import Http
+import googlemaps
 import json
-import urllib
-
-from oauth2client.service_account import ServiceAccountCredentials
 
 from django.views.generic import View, TemplateView
 from django.http import HttpResponse, JsonResponse
@@ -23,54 +19,23 @@ class HomeView(TemplateView):
 
 class AddAddress(View):
 
-    def _is_valid_location(self, lat, lng):
+    def _get_address(self, lat, lng):
         try:
-            conn = http.client.HTTPSConnection("maps.googleapis.com")
-            query_string = '{}?location={},{}&radius=500&key={}'.format(
-                settings.GOOGLE_GEO_URL, lat, lng, settings.GOOGLE_GEO_API_KEY)
-            conn.request("GET", query_string)
-            response = conn.getresponse()
-            data = json.loads(response.read().decode('utf-8'))
-            conn.close()
-            if data['status'] != 'ZERO_RESULTS':
-                return True
+            gmaps = googlemaps.Client(key=settings.GOOGLE_GEO_API_KEY)
+            reverse_geocode_result = gmaps.reverse_geocode((lat, lng))
+            return reverse_geocode_result[0]['formatted_address']
         except Exception as error:
             print(error)
-
-        return False
-
-    def _read_from_fusion(self, lat, lng):
-        scopes = ['https://www.googleapis.com/auth/fusiontables.readonly']
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            'AddressBook-777ffbfa3cdb.json', scopes=scopes)
-        http_auth = credentials.authorize(Http())
-
-        query_string = '{}?sql={}&key={}'.format(
-                'fusiontables/v2/query',
-                'SELECT * FROM 1z4oT18gGaoHWbnk08SV6hhOA99xC3OdheXulCjSW'.replace(' ', '%20'),
-                settings.GOOGLE_GEO_API_KEY)
-        resp, content_json = http_auth.request(
-            "https://www.googleapis.com/{}".format(query_string), "GET")
-        content = json.loads(content_json.decode('utf-8'))
-
-    def _insert_into_fusion(self, lat, lng):
-        scopes = ['https://www.googleapis.com/auth/fusiontables']
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            'AddressBook-777ffbfa3cdb.json', scopes=scopes)
-        http_auth = credentials.authorize(Http())
-        resp, content_json = http_auth.request(
-            "https://www.googleapis.com/fusiontables/v2/query",
-            "POST",
-            'sql=insert into 1z4oT18gGaoHWbnk08SV6hhOA99xC3OdheXulCjSW (location) values ({},{})'.format(lat,lng)
-        )
-        content = json.loads(content_json.decode('utf-8'))
+        return None
 
     def post(self, request, *args, **kwargs):
         lat, lng = request.POST.get('lat'), request.POST.get('lng')
-        #self._insert_into_fusion(lat, lng)
-        if (lat and lng) and self._is_valid_location(lat, lng):
-            Address.objects.create(latitude=lat, longitude=lng)
-            #self._insert_into_fusion(lat, lng)
+        formatted_address = self._get_address(lat, lng)
+        if formatted_address:
+            Address.objects.create(
+                latitude=lat,
+                longitude=lng,
+                address=formatted_address)
             results = Address.objects.values_list()
             return JsonResponse({'results':list(results)})
         else:
